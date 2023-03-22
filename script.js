359af7b7bab0c0a180eb31418538dea5307a9d1b
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-03-22 15:19:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) {
-    return [];
+function letterCombinations(input_digit) {
+  if (!input_digit.length) return [];
+
+  const mapping = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+  let result = [''];
+
+  for (const digit of input_digit) {
+    const letters = mapping[digit];
+    const temp = [];
+    for (const res of result) {
+      for (const letter of letters) {
+        temp.push(res + letter);
+      }
+    }
+    result = temp;
   }
 
-  const mappings = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz',
-  };
-
-  const result = [];
-
-  function recurse(index, current) {
-    if (index === digits.length) {
-      result.push(current);
-      return;
-    }
-
-    const letters = mappings[digits[index]];
-    for (let i = 0; i < letters.length; i++) {
-      recurse(index + 1, current + letters[i]);
-    }
-  }
-
-  recurse(0, '');
-  return result.sort();
+  return result;
 }
-
+module.exports = letterCombinations;
